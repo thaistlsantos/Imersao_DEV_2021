7cabb7f009df3781c075becbf7dddc2255aafec0
Thais de Lima Santos <thais_tlsan@hotmail.com>
2021-03-23 01:21:25 -0300

    up

---
+++ Desafio_01/conversao.js
@@ -0,0 +1,10 @@
+var valorDolar = prompt("Qual o valor em Dóllar que você deseja converter???")
+
+var valorDolarNum = parseFloat(valorDolar)
+
+alert(valorDolarNum)
+
+var valorReal = valorDolarNum * 5.50
+var valorRealDecimal = valorRealDecimal.toFixed(2)
+
+alert(valorRealDecimal)
